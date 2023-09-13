b2ab78bb0269db58c402a7455e0b8c64a0a583b5
Miguel Castro <miguelangelcastro@gmail.com>
2023-09-13 16:33:18 +0200

    Update installScript.sh

--- installScript.sh
+++ installScript.sh
@@ -7,7 +7,7 @@ echo ---------------------------------------
 echo
 echo This script installs:
 echo
-echo '-----> XNAT v.1.7.6'
+echo '-----> XNAT v.1.7'
 echo '-----> Tomcat Websever'
 echo '-----> Plugins & Pipelines'
 echo '-----> Testdata and Skripts'
