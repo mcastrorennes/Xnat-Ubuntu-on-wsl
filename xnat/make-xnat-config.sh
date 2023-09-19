8a179cd8335492fdb8504bfc19c3523086c53a50
Miguel Castro <miguelangelcastro@gmail.com>
2023-09-19 11:40:40 +0200

    Update make-xnat-config.sh

--- xnat/make-xnat-config.sh
+++ xnat/make-xnat-config.sh
@@ -19,11 +19,44 @@ spring.http.multipart.max-request-size=1073741824
 EOF
 fi
 
+# generate pipeline-engine config
+if [ ! -f $XNAT_ROOT/pipeline/xnat-pipeline-engine/gradle.properties ]; then
+  cat > $XNAT_ROOT/pipeline/xnat-pipeline-engine/gradle.properties << EOF
+xnatUrl=http://localhost
+siteName=XNAT
+adminEmail=admin@oldschool.edu
+smtpServer=mail.oldschool.edu
+destination=/data/xnat/pipeline
+EOF
+fi
 
 if [ ! -z "$XNAT_EMAIL" ]; then
   cat > $XNAT_HOME/config/prefs-init.ini << EOF
 [siteConfig]
 adminEmail=$XNAT_EMAIL
+EOF
+fi
+
+# generate ldap config
+if [ ! -f $XNAT_HOME/config/auth/ldap-provider.properties ]; then
+  cat > $XNAT_HOME/config/auth/ldap-provider.properties << EOF
+name=LDAP
+provider.id=ldap
+auth.method=ldap
+address=ldap://sample.sample.de/dc=uni-augsburg,dc=de
+userdn=cn=UNI-AUGSBURG\\mustermann,dc=uni-augsburg,dc=de
+password=
+search.base=
+search.filter=(sAMAccountName=mustermann)
+EOF
+fi
+
+# generate administer config
+if [ ! -f $XNAT_HOME/config/auth/local-provider.properties ]; then
+  cat > $XNAT_HOME/config/auth/local-provider.properties << EOF
+provider.db.name=LOCAL
+provider.db.id=localdb
+provider.db.type=db
 EOF
 fi
 
@@ -36,6 +69,44 @@ smtpPort=$XNAT_SMTP_PORT
 smtpUsername=$XNAT_SMTP_USERNAME
 smtpPassword=$XNAT_SMTP_PASSWORD
 smtpAuth=$XNAT_SMTP_AUTH
+EOF
+fi
+
+# generate local db config
+if [ ! -f $XNAT_HOME/config/prefs-init.ini ]; then
+  cat > $XNAT_HOME/config/prefs-init.ini << EOF
+[siteConfig]
+
+siteId=XNAT
+siteUrl=http://localhost
+adminEmail=fake@fake.fake
+
+archivePath=/data/xnat/archive
+prearchivePath=/data/xnat/prearchive
+cachePath=/data/xnat/cache
+buildPath=/data/xnat/build
+ftpPath=/data/xnat/ftp
+pipelinePath=/data/xnat/pipeline
+
+requireLogin=true
+userRegistration=false
+enableCsrfToken=true
+sessionTimeout=1 hour
+initialized=false
+
+[notifications]
+
+smtpEnabled=${SMTP_ENABLED}
+smtpHostname=${SMTP_HOSTNAME}
+smtpPort=${SMTP_PORT}
+smtpProtocol=${SMTP_PROTOCOL}
+smtpAuth=${SMTP_AUTH}
+smtpUsername=${SMTP_USERNAME}
+smtpPassword=${SMTP_PASSWORD}
+smtpStartTls=false
+smtpSslTrust=
+emailPrefix=XNAT
+
 EOF
 fi
 
